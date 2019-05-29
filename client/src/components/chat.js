e81d18e6f93b8419c2784d3b20a11a4b91fb3907
Xybolx <xybolx@gmail.com>
2019-05-29 07:30:56 -0700

    16th commit since build/eye gif change/chat.js changes

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -54,7 +54,6 @@ class Chat extends React.Component {
             } 
             clearTimeout(this.userJoinedTimeout);
             this.userJoinedTimeout = setTimeout(this.userJoiningTimeout, 4000);
-            // alert(`${data.user.username} has joined!`);
         });
 
         this.socket.on('RECEIVE_PRIVATE_MESSAGE', data => {
@@ -67,7 +66,6 @@ class Chat extends React.Component {
             }
             clearTimeout(this.sendPrivateMsgTimeout);
             this.sendPrivateMsgTimeout = setTimeout(this.sendingPrivateMsgTimeout, 4000);
-            // alert(`${data.author} sent you a private message!`);
         });
 
         this.socket.on('RECEIVE_TYPING_USER', data => {
@@ -103,58 +101,6 @@ class Chat extends React.Component {
 
         const addStatus = data => {
         };
-
-        // this.sendMessage = ev => {
-        //     ev.preventDefault();
-        //     this.resetLogOutTimeout();
-        //     if (this.state.message) {
-        //         API.saveMessage({
-        //             author: this.state.username,
-        //             userAvatar: this.state.userAvatar,
-        //             userColor: this.state.userColor,
-        //             message: this.state.message
-        //         });
-        //         this.socket.emit('SEND_MESSAGE', {
-        //             author: this.state.username,
-        //             userAvatar: this.state.userAvatar,
-        //             userColor: this.state.userColor,
-        //             message: this.state.message
-        //         });
-
-        //         this.socket.emit('SEND_STATUS', {
-        //             author: this.state.username
-        //         });
-        //     };
-
-        //     this.setState({ message: '' });
-        // };
-
-        // this.sendPrivateMessage = ev => {
-        //     ev.preventDefault();
-        //     this.resetLogOutTimeout();
-        //     let message = this.state.privateMessage.substr(1);
-        //     let ind = message.indexOf('/');
-        //     let receiver = message.substr(0, ind);
-        //     let messageIndex = message.substr(ind + 1);
-        //     if (this.state.privateMessage.substr(0, 1) === '@' && ind !== -1) {
-        //         API.savePrivateMessage({
-        //             receiver: receiver,
-        //             author: this.state.username,
-        //             userAvatar: this.state.userAvatar,
-        //             userColor: this.state.userColor,
-        //             privateMessage: messageIndex
-        //         });
-        //         this.socket.emit('SEND_PRIVATE_MESSAGE', {
-        //             receiver: receiver,
-        //             author: this.state.username,
-        //             userAvatar: this.state.userAvatar,
-        //             userColor: this.state.userColor,
-        //             privateMessage: messageIndex
-        //         });
-        //     };
-            
-        //     this.setState({ privateMessage: '' });
-        // };
 
         this.handleFormSubmit = ev => {
             ev.preventDefault();
@@ -372,10 +318,10 @@ class Chat extends React.Component {
                                      <input id="publicMsg" type="text" name="message" placeholder="📝Public Msg" className="form-control" value={this.state.message} onChange={this.handleInputChange} autoFocus />
                                      <br/>
                                      <button onClick={this.handleFormSubmit} className="btn btn-primary btn-block" type="button"><i className="far fa-paper-plane"></i>&nbsp;{this.state.msgSent ? `Sending...` : `Send` }</button>
-                                     <label id="privateMsgLabel" htmlFor="private message">Private Message</label>
+                                     {/* <label id="privateMsgLabel" htmlFor="private message">Private Message</label>
                                      <input id="privateMsg" type="text" name="privateMessage" placeholder="🔒Private Msg" className="form-control" value={this.state.privateMessage} onChange={this.handleInputChange} />
                                      <br />
-                                     <button onClick={this.sendPrivateMessage} className="btn btn-primary btn-block" type="button"><i className="far fa-paper-plane"></i> Send</button>
+                                     <button onClick={this.sendPrivateMessage} className="btn btn-primary btn-block" type="button"><i className="far fa-paper-plane"></i> Send</button> */}
                                      </form>
                                      <button onClick={this.logOut} className="btn btn-danger btn-block"> <i className="fas fa-user-slash"></i> Logout </button>
                                 </div>
