7d5f0b6dd2de7ba23b102d45747c2b4cc50b523c
Xybolx <xybolx@gmail.com>
2019-06-20 08:14:29 -0700

    added inside div class to users component

--- client/src/components/users.js
+++ client/src/components/users.js
@@ -5,9 +5,9 @@ import API from "../utils/API";
 
 class Users extends Component {
 
-        state = {
-            users: []
-        };
+    state = {
+        users: []
+    };
 
     handleMsgFocus = () => {
         document.getElementById("publicMsg").focus();
@@ -17,7 +17,7 @@ class Users extends Component {
         API.getUsers()
             .then(res =>
                 this.setState({ users: res.data }))
-                .catch(err => console.log(err))
+            .catch(err => console.log(err))
     };
 
     componentDidMount() {
@@ -32,27 +32,29 @@ class Users extends Component {
     render() {
         return (
             <div>
-                                {this.state.users.length ? (
-                                    <div className="users flex-fill text-center">
-                                    <h6><span className="fa-layers fa-fw"><i className="fas fa-users"></i><span className="fa-layers-counter" style={{ fontSize: 25 }}>{this.state.users.length}</span></span> Online Now</h6>
-                                        {this.state.users.map(user => (
-                                            <div key={user._id}>
-                                                <div style={{ borderColor: `${user.colorSeed}` }} className="card-header">
-                                                    <div className="userValue" onClick={this.handleMsgFocus} value={user.username} style={{ color: `${user.colorSeed}` }}>
-                                                        <img className="img-fluid" alt="" src={user.avatarURL}></img>&nbsp;{user.username} <span className="joinDate" style={{ fontSize: 8 }}>member since {moment(user.date).format("M/YYYY")}</span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        ))}
+                {this.state.users.length ? (
+                    <div className="users flex-fill text-center">
+                        <div className="inside">
+                            <h6><span className="fa-layers fa-fw"><i className="fas fa-users"></i><span className="fa-layers-counter" style={{ fontSize: 25 }}>{this.state.users.length}</span></span> Online Now</h6>
+                            {this.state.users.map(user => (
+                                <div key={user._id}>
+                                    <div style={{ borderColor: `${user.colorSeed}` }} className="card-header">
+                                        <div className="userValue" onClick={this.handleMsgFocus} value={user.username} style={{ color: `${user.colorSeed}` }}>
+                                            <img className="img-fluid" alt="" src={user.avatarURL}></img>&nbsp;{user.username} <span className="joinDate" style={{ fontSize: 8 }}>member since {moment(user.date).format("M/YYYY")}</span>
+                                        </div>
                                     </div>
-                                ) : (
-                                        <Sound
-                                            url="welcome.wav"
-                                            playStatus={Sound.status.PLAYING}
-                                            playbackRate={.75} 
-                                        />
-                                    )}
-                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ) : (
+                        <Sound
+                            url="welcome.wav"
+                            playStatus={Sound.status.PLAYING}
+                            playbackRate={.75}
+                        />
+                    )}
+            </div>
         );
     };
 };
