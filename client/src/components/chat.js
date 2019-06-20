727625fa9b0aeff2ca13719b8304eb457c05704c
Xybolx <xybolx@gmail.com>
2019-06-20 15:53:13 -0700

    moved clearTimeouts from receive events

--- client/src/components/chat.js
+++ client/src/components/chat.js
@@ -57,10 +57,9 @@ class Chat extends Component {
         this.socket.on('RECEIVE_USER_JOINED', data => {
             const joiningUser = `${data.user.username}`;
             if (data && this.state.userJoining !== joiningUser) {
-                this.setState({ joiningUsers: [...this.state.joiningUsers, data], userJoining: joiningUser })
+                this.setState({ joiningUsers: [...this.state.joiningUsers, data], userJoining: joiningUser });
+                this.userJoiningTimeout();
             } 
-            clearTimeout(this.userJoinedTimeout);
-            this.userJoinedTimeout = setTimeout(this.userJoiningTimeout, 4000);
         });
 
         this.socket.on('RECEIVE_USER_LEFT', data => {
@@ -95,9 +94,8 @@ class Chat extends Component {
         this.socket.on('RECEIVE_TYPING_USER', data => {
             if (data && !this.state.userTyping) {
                 this.setState({ typingUsers: [...this.state.typingUsers, data], userTyping: data.username });
+                this.typingTimeout();
             }
-            clearTimeout(this.typeTimeout);
-            this.typeTimeout = setTimeout(this.typingTimeout, 4000);
         });
 
         this.socket.on('RECEIVE_CLEAR_MSGS', data => {
@@ -296,6 +294,8 @@ class Chat extends Component {
     };
 
     userJoiningTimeout = () => {
+        clearTimeout(this.userJoinedTimeout);
+        this.userJoinedTimeout = setTimeout(this.userJoiningTimeout, 4000);
         this.setState({ joiningUsers: [], userJoining: '' });
     };
 
@@ -304,6 +304,8 @@ class Chat extends Component {
     };
 
     typingTimeout = () => {
+        clearTimeout(this.typeTimeout);
+        this.typeTimeout = setTimeout(this.typingTimeout, 4000);
         this.setState({ typingUsers: [], userTyping: '' });
     };
 
